ded8e2b2a692ef3b9dc1af0d5dcac98e99728136
Jim King <jeking@wheatonwebsiteservices.com>
2015-04-17 09:40:30 -0500

    Initial Commit of v 2.0

---
+++ includes/languages/czech/YOUR_CUSTOM_TEMPLATE/product_info.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * @package languageDefines
+ * @copyright Copyright 2003-2006 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: product_info.php 3159 2006-03-11 01:35:04Z drbyte $
+ *
+ * Modified by Pavel Palek (2P) aka Dedek (zencart@palek.net) - 2007-03-19 - Average Product Rating
+ */
+
+define('TEXT_PRODUCT_NOT_FOUND', 'Omlouváme se, ale v souèasné dobì zde není žádné zboží.');
+define('TEXT_CURRENT_REVIEWS', 'Poèet hodnocení:');
+define('TEXT_MORE_INFORMATION', 'Pro více informací o zboží navštivte <a href="%s" target="_blank">domovskou stránku</a>.');
+define('TEXT_DATE_ADDED', 'Tento produkt byl pøidán dne %s.');
+define('TEXT_DATE_AVAILABLE', 'Toto zboží bude skladem dne %s.');
+define('TEXT_ALSO_PURCHASED_PRODUCTS', 'Zákaznící, kteøí si koupili toto zboží, objednávali také...');
+define('TEXT_PRODUCT_OPTIONS', 'Vyberte prosím: ');
+define('TEXT_PRODUCT_MANUFACTURER', 'Výrobce: ');
+define('TEXT_PRODUCT_WEIGHT', 'Pøepravní váha: ');
+define('TEXT_PRODUCT_QUANTITY', ' Balení skladem');
+define('TEXT_PRODUCT_MODEL', 'Kód: ');
+
+
+
+// previous next product
+define('PREV_NEXT_PRODUCT', 'Zboží ');
+define('PREV_NEXT_FROM', ' z ');
+define('IMAGE_BUTTON_PREVIOUS','Pøedchozí');
+define('IMAGE_BUTTON_NEXT','Další');
+define('IMAGE_BUTTON_RETURN_TO_PRODUCT_LIST','Zpìt na výbìr zboží');
+
+// missing products
+//define('TABLE_HEADING_NEW_PRODUCTS', 'Nové zboží pro %s');
+//define('TABLE_HEADING_UPCOMING_PRODUCTS', 'Naše nabídka');
+//define('TABLE_HEADING_DATE_EXPECTED', 'Date Expected');
+
+define('TEXT_ATTRIBUTES_PRICE_WAS',' [byla: ');
+define('TEXT_ATTRIBUTE_IS_FREE',' nyní je: Zdarma]');
+define('TEXT_ONETIME_CHARGE_SYMBOL', ' *');
+define('TEXT_ONETIME_CHARGE_DESCRIPTION', ' Jednorázové poplatky lze použít');
+define('TEXT_ATTRIBUTES_QTY_PRICE_HELP_LINK','Množstevní slevy dostupné');
+define('ATTRIBUTES_QTY_PRICE_SYMBOL', zen_image(DIR_WS_TEMPLATE_ICONS . 'icon_status_green.gif', TEXT_ATTRIBUTES_QTY_PRICE_HELP_LINK, 10, 10) . '&nbsp;');
+
+define('TEXT_CURRENT_REVIEWS_RATING', 'Prùmìrné hodnocení:'); // 2P added - Average Product Rating
+?>
